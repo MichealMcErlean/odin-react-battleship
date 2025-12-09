cbc10460111ba7f0a49dd5823a94f53d3cea30ab
Micheal McErlean <MichealMcErlean@users.noreply.github.com>
2025-12-10 09:49:33 +1100

    Contexts are in place
    PlayerContext to cover the human Player, ComputerContext for the computer Player.  No prop drilling required for those objects now.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
+import { createContext, useContext, useState} from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router';
 import './index.css'
 import App from './App.jsx'
+import {Ship, GameBoard, Player } from './scripts/ship.js';
+import GamePage from './routes/GamePage.jsx'
+
+const PlayerContext = createContext(null);
+const ComputerContext = createContext(null);
+
+function PlayerProvider({children}) {
+  const [player, setPlayer] = useState(new Player('human'));
+  return (
+    <PlayerContext.Provider value={{player, setPlayer}}>
+      {children}
+    </PlayerContext.Provider>
+  );
+}
+
+function ComputerProvider({children}) {
+  const [computer, setComputer] = useState(new Player('computer'));
+  return (
+    <ComputerContext.Provider value = {{computer, setComputer}}>
+      {children}
+    </ComputerContext.Provider>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/game",
+    element: <GamePage />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PlayerProvider>
+    <ComputerProvider>
+      <RouterProvider router={router} />
+    </ComputerProvider>
+  </PlayerProvider>
 )
---
+++ src/routes/GamePage.jsx
@@ -0,0 +1,8 @@
+import styles from './GamePage.module.css';
+
+export default function GamePage() {
+
+  return (
+    <h1>Hello too!</h1>
+  )
+}
