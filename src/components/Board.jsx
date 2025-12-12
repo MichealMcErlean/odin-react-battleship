d1827304763a4359dccb12e8e55c59a2616e86e5
Micheal McErlean <MichealMcErlean@users.noreply.github.com>
2025-12-12 11:56:12 +1100

    Boards showing on load of GamePage
    Ready for play

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -5,22 +5,27 @@ import { usePlayer } from '../contexts/PlayerContext.jsx';
 import { useComputer } from '../contexts/ComputerContext.jsx';
 
 export default function Board({onClick, type}) {
-  let player;
-  let setPlayer;
 
-  if (type == 'human') {
-    ({player, setPlayer} = usePlayer());
+  let currentPlayer;
+  let setCurrentPlayer;
+
+  if (type === 'human') {
+    ({player: currentPlayer, setPlayer: setCurrentPlayer} = usePlayer());
   } else {
-    ({player, setPlayer} = useComputer());
+    ({computer: currentPlayer, setComputer: setCurrentPlayer} = useComputer());
+  }
+
+  if (!currentPlayer || !currentPlayer.board) {
+    return <div>Loading board...</div>
   }
 
   return (
     <div className={styles.container}>
-      {player.board.map((row, rowI) => (
+      {currentPlayer.board.map((row, rowI) => (
         row.map((cell, colI) => (
           <Cell 
-            isHit={player.board[rowI][colI].hit}
-            contains={player.board[rowI][colI].ship}
+            isHit={currentPlayer.board[rowI][colI].hit}
+            contains={currentPlayer.board[rowI][colI].ship}
             type={type}
             onClick={(e) => onClick(e, rowI, colI)}
             key={`${rowI}-${colI}`}
--- src/routes/GamePage.jsx
+++ src/routes/GamePage.jsx
@@ -1,22 +1,46 @@
 import styles from './GamePage.module.css';
+import { usePlayer } from '../contexts/PlayerContext';
+import { useComputer } from '../contexts/ComputerContext';
+import Board from '../components/Board';
+import { useState } from 'react';
 
 export default function GamePage() {
 
+  const {player, setPlayer} = usePlayer();
+  const {computer, setComputer} = useComputer();
+
+  const [msglog, setMsglog] = useState([(<p>Hostilities commenced!</p>), (<p>Open fire!</p>)]);
+
+  function handlePlayerAttack(event) {
+    return;
+  }
+
   return (
-    <main>
+    <main className = {styles.main}>
       <header className={styles.header}>
         <div className={styles.msglog}>
-          <p>Hostilities commenced!</p>
-          <p>Open fire!</p>
+          {msglog}
         </div>
       </header>
       <article className={styles.article}>
         <div className={styles.boardbox}>
-          <div className={styles.computerboard}>
-            Placeholder
+          <div className={styles.boardAndTitle}>
+            <h2>Enemy Waters</h2>
+            <div className={styles.computerboard}>
+              <Board
+                onClick={handlePlayerAttack}
+                type='computer'
+              />
+            </div>
           </div>
-          <div className={styles.playerboard}>
-            Placeholder
+          <div className={styles.boardAndTitle}>
+            <h2>Friendly Waters</h2>
+            <div className={styles.playerboard}>
+              <Board
+                onClick=''
+                type='human'
+              />
+            </div>
           </div>
         </div>
         <div className = {styles.buttonbox}>
